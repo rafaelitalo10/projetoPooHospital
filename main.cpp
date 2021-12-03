71342b5fb6bb7d74658e62da1b7842657618c467
Mariana D. P. Lima <marianaprata97@gmail.com>
2021-12-03 15:56:14 -0300

    Separação das classes

--- main.cpp
+++ main.cpp
@@ -9,18 +9,5 @@ using namespace std;
 
 int main()
 {
-    Leito leito(false);
-    leito.set_ocupacao(true);
-    cout << leito.get_ocupacao() <<endl;
-    leito.set_ocupacao(false);
-    cout << leito.get_ocupacao() <<endl;
-
-
-    Paciente pessoa("0585696586985", "Teobaldo", "ruA JFJHEWJFEF", "85483457458487", "A", 87);
-    cout << pessoa.get_paciente()<<endl;
-
-    ArquivoPacientes aqr();
-
-
     return 0;
 }
